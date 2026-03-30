7010f91290c6272dcd6d31248e037e511b312c18
raffaello <raffaelloclarck@gmail.com>
2026-03-30 13:03:50 +0200

    Initial Commit

---
+++ True Road Adventures/True_Road_AdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  True_Road_AdventuresApp.swift
+//  True Road Adventures
+//
+//  Created by Raffaello Clarck on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct True_Road_AdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
